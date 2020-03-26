53a40084349439db51cd212c54a96fee9bf1c972
Edvin <edvin.bourelius@gmail.com>
2020-03-26 13:53:58 +0100

    Remove points close together :

--- Template matching/myTemplateMatcher.m
+++ Template matching/myTemplateMatcher.m
@@ -15,6 +15,21 @@ function corners = myTemplateMatcher(im,template)
     offset = corrPeaks-corr_offset;
     corrPeaks = corrPeaks - offset./2;
     
+    %corrPeaks = uniquetol(corrPeaks,'ByRows',100);
+    for i = 1:1:length(corrPeaks)
+        peak = corrPeaks(i,:);
+        for n = i+1:1:length(corrPeaks)
+            currentPeak = corrPeaks(n,:);
+            diff = norm(currentPeak - peak);
+            if diff < 100
+                corrPeaks(n,:) = [-1 -1];
+            end
+            lastPeak = currentPeak;
+        end
+    end
+    corrPeaks(corrPeaks == [-1 -1]) = [ ];
+    corrPeaks = reshape(corrPeaks,size(corrPeaks,2)/2,2);
+    
     corners = corrPeaks;
     corners = cornerPoints(corners);
 end
