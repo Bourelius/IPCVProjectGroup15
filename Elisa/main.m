40670966ea2dd16f7b7489113e3a55318c3c145c
elisa <elisa.nguyen@live.de>
2020-04-07 11:08:38 +0200

    side classification

--- Elisa/main.m
+++ Elisa/main.m
@@ -1,14 +1,10 @@
 clear all;
 close all;
 
-addpath('./Template matching/');
-addpath('./Videos/');
-addpath('./Preprocessing/');
-
 banner = imread('../UT_Logo_Black_EN.jpg');
 
 %% Read the video
-vid = VideoReader('../Videos/india_bangladesh_1.mp4');
+vid = VideoReader('../Videos/video3.mp4');
 output = VideoWriter('out.mp4','MPEG-4');
 videoPlayer = vision.VideoPlayer();
 vid.CurrentTime = 1;
@@ -18,7 +14,11 @@ vid.CurrentTime = 1;
 while hasFrame(vid)
    frame1 = readFrame(vid);
    frame = myROISelector(frame1);
-   figure(2); imshow(frame, []);
+   figure(30);imshow(frame);
+   
+   side = mySideClassifier(frame);
+   
+   
    [corners, theta] = myIntersectionFinder(frame);
    out=myInsertBanner(corners,frame1, theta);
         bannerPoints = [0,0;
