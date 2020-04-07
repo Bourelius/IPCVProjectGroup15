a6ff6ced65c1ad9c4d93bae4fa056e622fd6521d
elisa <elisa.nguyen@live.de>
2020-04-07 22:39:48 +0200

    correction of angle calculation left

--- Elisa/myAnglePointCalculatorLeft.m
+++ Elisa/myAnglePointCalculatorLeft.m
@@ -2,12 +2,13 @@ function [new_corner] = myAnglePointCalculatorLeft(theta, bottom_corner, top_cor
 %MYANGLEPOINTCALCULATORLEFT calculating banner points to go up 60°
 %   Detailed explanation goes here
     a = norm(bottom_corner - top_corner) / 2; 
-    x = a * sind(180-theta);
-    i = x / sind(1/3*theta);
-    h = i * cosd(1/3*theta);
+    x = a * sind(2/3*(180-theta));
+    h = x / sind(1/3*(180-theta));
+    i = a * sind(90-(180-theta));
+    j = a * cosd(90-(180-theta));
     
-    new_corner(1) = top_corner(1) - h;
-    new_corner(2) = top_corner(2) + x;
+    new_corner(1) = top_corner(1) + i - h;
+    new_corner(2) = top_corner(2) + j;
 
 end
 
